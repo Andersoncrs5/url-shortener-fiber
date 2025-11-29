ed5dc6563afef7434760b4c9d324465b4eee9191
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-28 21:28:35 -0300

    change size column SHORT_CODE

--- write-api/models/links.go
+++ write-api/models/links.go
@@ -4,7 +4,7 @@ import "time"
 
 type Links struct {
 	ID         int64      `json:"id" gorm:"primaryKey;type:bigint;not null"`
-	SHORT_CODE string     `json:"short_code" gorm:"type:varchar(10);uniqueIndex;not null"`
+	SHORT_CODE string     `json:"short_code" gorm:"type:varchar(12);uniqueIndex;not null"`
 	LONG_URL   string     `json:"long_url" gorm:"type:text;not null"`
 	CreatedAt  time.Time  `json:"created_at"`
 	ExpiresAt  *time.Time `json:"expires_at"`
