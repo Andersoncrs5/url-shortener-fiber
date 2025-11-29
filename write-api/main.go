16638e2bf6cf5a21ad887b1993a3e6088cd8d7fc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-29 15:31:07 -0300

    Applied linkHandler

--- write-api/main.go
+++ write-api/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"linkfast/write-api/configs"
+	"linkfast/write-api/handlers"
+	"linkfast/write-api/repositories"
+	"linkfast/write-api/routers"
+	"linkfast/write-api/services"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,6 +14,13 @@ func main() {
 	app := fiber.New()
 
 	configs.ConnectDB()
+	db := configs.DB
+
+	linkRepository := repositories.NewLinkRepository(db)
+	linkService := services.NewUserService(linkRepository)
+	linkHandler := handlers.NewTaskHandler(linkService)
+
+	routers.LinkRoute(app, linkHandler)
 
 	app.Listen(":8888")
 }
