8fda8f94aed115bb277f267c00a012e6552bbdb7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-30 12:16:47 -0300

    Improvements in methods

--- write-api/services/link_service.go
+++ write-api/services/link_service.go
@@ -11,12 +11,12 @@ import (
 )
 
 type LinkService interface {
-	Create(dto dtos.CreateLinkDto) (*models.Links, error, int)
-	GetByID(id int64) (*models.Links, error, int)
+	Create(dto dtos.CreateLinkDto) (*models.Links, error)
+	GetByID(id int64) (models.Links, error)
 	ExistsByID(id int64) (bool, error)
-	GetByShotCode(code string) (*models.Links, error, int)
-	ExistsByShotCode(code string) (bool, error, int)
-	Delete(link *models.Links) (error, int)
+	GetByShotCode(code string) (*models.Links, error)
+	ExistsByShotCode(code string) (bool, error)
+	Delete(link *models.Links) error
 }
 
 type linkService struct {
@@ -29,18 +29,18 @@ func NewUserService(repo repositories.LinkRepository) LinkService {
 	}
 }
 
-func (l *linkService) Create(dto dtos.CreateLinkDto) (*models.Links, error, int) {
-	link := models.Links{}
+func (l *linkService) Create(dto dtos.CreateLinkDto) (*models.Links, error) {
+	link := new(models.Links)
 
-	if err := copier.Copy(dto, &link); err != nil {
-		log.Printf("Error the copier CreateLinkDto to Links: %v", err)
-		return nil, consts.ErrInternal, 500
+	if err := copier.Copy(link, dto); err != nil {
+		log.Printf("Error copying CreateLinkDto to Links model: %v", err)
+		return nil, consts.ErrInternal
 	}
 
-	return l.repo.Create(link)
+	return l.repo.Create(*link)
 }
 
-func (l *linkService) GetByID(id int64) (*models.Links, error, int) {
+func (l *linkService) GetByID(id int64) (models.Links, error) {
 	return l.repo.GetByID(id)
 }
 
@@ -48,14 +48,14 @@ func (l *linkService) ExistsByID(id int64) (bool, error) {
 	return l.repo.ExistsByID(id)
 }
 
-func (l *linkService) GetByShotCode(code string) (*models.Links, error, int) {
+func (l *linkService) GetByShotCode(code string) (*models.Links, error) {
 	return l.repo.GetByShotCode(code)
 }
 
-func (l *linkService) ExistsByShotCode(code string) (bool, error, int) {
+func (l *linkService) ExistsByShotCode(code string) (bool, error) {
 	return l.repo.ExistsByShotCode(code)
 }
 
-func (l *linkService) Delete(link *models.Links) (error, int) {
+func (l *linkService) Delete(link *models.Links) error {
 	return l.repo.Delete(link)
 }
