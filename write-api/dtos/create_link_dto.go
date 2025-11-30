b2977c891aa88589d674a6dc76b4dbe9f783887c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-30 12:18:44 -0300

    Added validate

--- write-api/dtos/create_link_dto.go
+++ write-api/dtos/create_link_dto.go
@@ -3,6 +3,6 @@ package dtos
 import "time"
 
 type CreateLinkDto struct {
-	LONG_URL  string     `json:"long_url"`
-	ExpiresAt *time.Time `json:"expires_at"`
+	LONG_URL  string     `json:"long_url" validate:"required,min=8,max=2500"`
+	ExpiresAt *time.Time `json:"expires_at" validate:"omitempty,gt=now"`
 }
