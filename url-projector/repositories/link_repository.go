8057a9f490011cda5e308145f63c7b921f4573c9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-30 20:21:50 -0300

    Impled LinkRepository and your methods

--- url-projector/repositories/link_repository.go
+++ url-projector/repositories/link_repository.go
@@ -1,8 +1,20 @@
 package repositories
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"errors"
+	models "linkfast/url-projector/model"
+	"linkfast/write-api/utils/consts"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type LinkRepository interface {
+	GetByCode(ctx context.Context, code string) (models.Link, error)
+	GetById(ctx context.Context, id string) (models.Link, error)
+	Delete(ctx context.Context, id string) error
+	Create(ctx context.Context, link models.Link) (models.Link, error)
 }
 
 type linkRepository struct {
@@ -15,6 +27,57 @@ func NewLinkRepository(db *mongo.Database) LinkRepository {
 	}
 }
 
-func (l *linkRepository) GetByCode(code string) {
+func (l *linkRepository) GetByCode(ctx context.Context, code string) (models.Link, error) {
+	var link models.Link
 
+	filter := bson.M{"short_code": code}
+
+	if err := l.collection.FindOne(ctx, filter).Decode(&link); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return link, consts.ErrRecordNotFound
+		}
+
+		return link, consts.ErrInternal
+	}
+
+	return link, nil
+}
+
+func (l *linkRepository) GetById(ctx context.Context, id string) (models.Link, error) {
+	var link models.Link
+
+	filter := bson.M{"_id": id}
+
+	if err := l.collection.FindOne(ctx, filter).Decode(&link); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return link, consts.ErrRecordNotFound
+		}
+
+		return link, consts.ErrInternal
+	}
+
+	return link, nil
+}
+
+func (l *linkRepository) Delete(ctx context.Context, id string) error {
+	filter := bson.M{"_id": id}
+	result, err := l.collection.DeleteOne(ctx, filter)
+
+	if err != nil {
+		return err
+	}
+
+	if result.DeletedCount == 0 {
+		return consts.ErrRecordNotFound
+	}
+
+	return nil
+}
+
+func (l *linkRepository) Create(ctx context.Context, link models.Link) (models.Link, error) {
+	if _, err := l.collection.InsertOne(ctx, link); err != nil {
+		return link, consts.ErrInternal
+	}
+
+	return link, nil
 }
