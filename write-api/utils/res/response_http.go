24d7d6cb78bd3b4a80f7cd109a80841af70a2335
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-30 12:17:00 -0300

    chane type column Code

--- write-api/utils/res/response_http.go
+++ write-api/utils/res/response_http.go
@@ -5,7 +5,7 @@ import "time"
 type ResponseHttp[T any] struct {
 	Timestamp time.Time `json:"timestamp"`
 	Payload   T         `json:"payload"`
-	Code      int16     `json:"code"`
+	Code      int       `json:"code"`
 	Status    bool      `json:"status"`
 	Message   string    `json:"message"`
 	Version   uint8     `json:"version"`
