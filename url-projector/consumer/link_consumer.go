1b2cd447c8451fdce9c868b1447a6a6f1f81d096
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-30 21:47:13 -0300

    Config consumer with kafka

---
+++ url-projector/consumer/link_consumer.go
@@ -0,0 +1,47 @@
+package consumer
+
+import (
+	"log"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+var consumerGroupID = "link_fast_group"
+
+func LinkConsumer(brokers, topic string) {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  brokers,
+		"group.id":           consumerGroupID,
+		"auto.offset.reset":  "latest",
+		"enable.auto.commit": true,
+	})
+
+	if err != nil {
+		log.Fatalf("Falha ao criar o consumer Kafka: %v", err)
+	}
+
+	defer consumer.Close()
+
+	if err_subscribe := consumer.Subscribe(topic, nil); err_subscribe != nil {
+		log.Fatalf("Falha ao se inscrever no tópico %s: %v", topic, err_subscribe)
+	}
+
+	for {
+		msg, err := consumer.ReadMessage(time.Second)
+		if err == nil {
+
+			log.Printf("Mensagem recebida do Tópico %s [%d] em Offset %v",
+				*msg.TopicPartition.Topic,
+				msg.TopicPartition.Partition,
+				msg.TopicPartition.Offset)
+
+			log.Print(msg.Key)
+
+		} else {
+			if kafkaErr, ok := err.(kafka.Error); ok && kafkaErr.Code() != kafka.ErrTimedOut {
+				log.Printf("Erro do consumer: %v\n", err)
+			}
+		}
+	}
+}
