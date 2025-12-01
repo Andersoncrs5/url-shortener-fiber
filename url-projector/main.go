2c994fb66ad944323dd5a12fd98e5389738dd81d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-01 19:45:07 -0300

    Added connection with mongo

--- url-projector/main.go
+++ url-projector/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	configs "linkfast/url-projector/config"
 	"linkfast/url-projector/consumer"
+	"linkfast/url-projector/repositories"
+	"linkfast/url-projector/services"
 	"linkfast/url-projector/utils/envs"
 	"log"
 	"time"
@@ -15,10 +19,14 @@ func main() {
 
 	kafkaBrokers := envs.GetEnvWithFallback("KAFKA_BROKERS", "")
 	kafkaTopic := envs.GetEnvWithFallback("KAFKA_TOPIC", "")
+	mongoURI := envs.GetEnvWithFallback("MONGO_URI", "")
+	mongoDBName := envs.GetEnvWithFallback("MONGO_DB_NAME", "")
 
 	required := map[string]string{
-		"kafkaBrokers": kafkaBrokers,
-		"kafkaTopic":   kafkaTopic,
+		"KAFKA_BROKERS": kafkaBrokers,
+		"KAFKA_TOPIC":   kafkaTopic,
+		"MONGO_URI":     mongoURI,
+		"MONGO_DB_NAME": mongoDBName,
 	}
 
 	for key, value := range required {
@@ -27,6 +35,27 @@ func main() {
 		}
 	}
 
+	mongoCfg := configs.MongoConfig{
+		URI:    mongoURI,
+		DBName: mongoDBName,
+	}
+	mongoClient, err := configs.InitMongoDBConnection(mongoCfg)
+	if err != nil {
+		log.Fatalf("Falha crítica ao conectar ao MongoDB: %v", err)
+	}
+
+	defer func() {
+		if err = mongoClient.Disconnect(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	mongoDB := mongoClient.Database(mongoDBName)
+
+	linkRepo := repositories.NewLinkRepository(mongoDB)
+
+	linkService := services.NewLinkService(linkRepo)
+
 	log.Println("Reading topics!")
-	consumer.LinkConsumer(kafkaBrokers, kafkaTopic)
+	consumer.LinkConsumer(kafkaBrokers, kafkaTopic, linkService)
 }
