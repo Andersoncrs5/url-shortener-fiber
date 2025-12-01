90249002113c7baca4528488eaf642103b3a40c4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-01 20:13:49 -0300

    New methods ExistsByShortCode and ExistsByID

--- read-api/repositories/link_repository.go
+++ read-api/repositories/link_repository.go
@@ -3,8 +3,9 @@ package repositories
 import (
 	"context"
 	"errors"
-	models "linkfast/url-projector/model"
-	"linkfast/url-projector/utils/consts"
+	"linkfast/read-api/models"
+	"linkfast/read-api/utils/consts"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,7 +13,9 @@ import (
 
 type LinkRepository interface {
 	GetByCode(ctx context.Context, code string) (models.Link, error)
+	ExistsByShortCode(ctx context.Context, code string) (bool, error)
 	GetById(ctx context.Context, id int64) (models.Link, error)
+	ExistsByID(ctx context.Context, id int64) (bool, error)
 }
 
 type linkRepository struct {
@@ -55,4 +58,30 @@ func (l *linkRepository) GetById(ctx context.Context, id int64) (models.Link, er
 	}
 
 	return link, nil
+}
+
+func (l *linkRepository) ExistsByID(ctx context.Context, id int64) (bool, error) {
+	filter := bson.M{"_id": id}
+
+	count, err := l.collection.CountDocuments(ctx, filter)
+
+	if err != nil {
+		log.Printf("error counting records by ID %d: %v", id, err)
+		return false, consts.ErrInternal
+	}
+
+	return count > 0, nil
+}
+
+func (l *linkRepository) ExistsByShortCode(ctx context.Context, code string) (bool, error) {
+	filter := bson.M{"short_code": code}
+
+	count, err := l.collection.CountDocuments(ctx, filter)
+
+	if err != nil {
+		log.Printf("error counting records by ShortCode %s: %v", code, err)
+		return false, consts.ErrInternal
+	}
+
+	return count > 0, nil
 }
