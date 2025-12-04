ed7e9268c495ef8d1f91e327986791880d7ec49c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-04 20:36:00 -0300

    Configured routers

---
+++ read-api/routers/link_routers.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"linkfast/read-api/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func LinkRoute(app *fiber.App, linkHandler handlers.LinkHandler) {
+	router := app.Group("/api/v1/links")
+
+	router.Get("/:id", linkHandler.GetByID)
+	router.Get("/:code/code", linkHandler.GetByShotCode)
+}
