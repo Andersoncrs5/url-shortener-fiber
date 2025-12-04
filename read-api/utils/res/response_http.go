b95784bd1ef5ba4759fae7387e7a9b0105121f5c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-04 20:35:42 -0300

    Added ResponseHttp

---
+++ read-api/utils/res/response_http.go
@@ -0,0 +1,14 @@
+package res
+
+import "time"
+
+type ResponseHttp[T any] struct {
+	Timestamp time.Time `json:"timestamp"`
+	Payload   T         `json:"payload"`
+	Code      int       `json:"code"`
+	Status    bool      `json:"status"`
+	Message   string    `json:"message"`
+	Version   uint8     `json:"version"`
+	TraceID   string    `json:"trace_id,omitempty"`
+	Path      string    `json:"path,omitempty"`
+}
